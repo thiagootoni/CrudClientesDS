9dc743417d3f43a45226c6a18a2d6823b80af070
Thiago Otoni <39462973+thiagootoni@users.noreply.github.com>
2021-02-13 12:08:04 -0300

    Update: Estrutura Inicial

---
+++ src/main/java/com/thiago/clientes/CrudClientesDsApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClientesDsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiago/clientes/CrudClientesDsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesDsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
